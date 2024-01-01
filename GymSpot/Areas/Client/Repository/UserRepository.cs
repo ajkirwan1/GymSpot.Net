802e880f1105a1078f13eb34d6b2c3e579c06fed
Adam Kirwan <ajkirwan1@gmail.com>
2024-01-01 20:32:01 +0000

    Updated User database

---
+++ GymSpot/Areas/Client/Repository/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using GymSpot.Areas.Client.Models;
+using GymSpot.Data;
+
+namespace GymSpot.Areas.Client.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public UserRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public UserModel? GetUser()
+        {
+            var user = _applicationDbContext.Users.FirstOrDefault(user => user.UserId == 1);
+
+            return user;
+        }
+
+        public void Update(UserModel user)
+        {
+            _applicationDbContext.Users.Update(user);
+            _applicationDbContext.SaveChanges();
+        }
+
+
+    }
+}
---
+++ GymSpot/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using GymSpot.Areas.Client.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymSpot.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>().HasData(
+             new UserModel
+             {
+                 UserId = 1,
+                 Name = "Adam Kirwan",
+                 DateofBirth = DateTime.Now,
+                 AccountPassword = "123",
+                 Address = "Some Address",
+                 EmailAddress = "Adam@123Kirwan",
+                 Gender = Gender.Male,
+             }
+           );
+        }
+
+    }
+}
