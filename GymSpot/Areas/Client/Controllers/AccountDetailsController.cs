9843e08a224e83bbf5587202f4f826da0ec302e9
Adam Kirwan <ajkirwan1@gmail.com>
2024-01-01 12:36:12 +0000

    Squashed commit

---
+++ GymSpot/Areas/Client/Controllers/AccountDetailsController.cs
@@ -0,0 +1,18 @@
+﻿using GymSpot.Areas.Client.Data;
+using GymSpot.Areas.Client.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymSpot.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class AccountDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            MockUserData MockClientDetails = new MockUserData();
+
+            UserModel userModel = MockClientDetails.GetUserModel();
+            return View(userModel);
+        }
+    }
+}
---
+++ GymSpot/Areas/Client/Data/MockClientData.cs
@@ -0,0 +1,30 @@
+﻿using GymSpot.Areas.Client.Models;
+
+namespace GymSpot.Areas.Client.Data
+{
+    public class MockUserData
+    {
+
+        private readonly UserModel _mockUserModel;
+
+        public MockUserData()
+        {
+            _mockUserModel = new UserModel()
+            {
+                Name = "Adam Kirwan",
+                DateofBirth = DateTime.Now,
+                AccountPassword = "123",
+                Address = "Some Address",
+                EmailAddress = "Adam@123",
+                Gender = Gender.Male,
+            };
+        }
+
+
+        public UserModel GetUserModel()
+        {
+            return _mockUserModel;
+        }
+
+    }
+}
